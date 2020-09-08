b2de7e7f77199743e1ac64ba08abbfbc44e3b141
Marudhupandi <marudhupandi25697@gmail.com>
2020-09-08 19:13:13 +0530

    Initial commit

---
+++ sample.php
@@ -0,0 +1,74 @@
+<?php // Print out to the screen
+          /*echo "Hello World!<br/>";
+
+          $first_name = "Marudhu";
+          $fav_number = 7;
+
+          echo $first_name;
+          echo $fav_number; */
+
+          //Math Operator
+          // + - * / %
+
+          //echo 41 + 4;
+
+          //String Concontenation
+          /*$first_name = "Marudhu";
+          $last_name = "Pandi";
+
+          echo $first_name . " " . $last_name;
+          echo "<br/>";
+          echo $first_name . "<br/>" . $last_name;*/
+
+          //Arrays - Numeric
+
+          /*$first_names = array("Marudhu", "pandi");
+          echo $first_names[1];*/
+
+          //Arrays - Associative
+
+          /*$fav_food = array(
+            "Marudhu"=>"Hyderabad Briyani",
+            "Pandi"=>"Egg Rice"
+          );
+
+          echo $fav_food["Marudhu"];
+          echo "<br/>";
+          echo count($fav_food);*/
+        
+          //Array count
+
+          /*$names = array("Marudhu" , "Muthu" , "Kogila" , "Govindan" , "Sandhosam");
+
+          echo $names[count($names)-1];*/
+          //While Loop
+          /*$counter = 0;
+          while ($counter <= 10){
+          	echo "The Count is : $counter<br/>";
+          	$counter++;
+          };*/
+
+          //For loop
+
+          /*for($count = 0 ; $count<=10 ; $count++){
+          	echo "The count is : $count<br/>";
+          };*/
+
+          //foreach Loops
+          /*$names  = array("Marudhu" , "Sandhosam" , "Muthu");
+          foreach ($names as $value) {
+          	echo "$value<br/>";
+          }*/
+
+          //Functions
+
+          /*function helloThere(){
+          	echo "Hello There!";
+          };
+
+          helloThere();*/
+
+          //echo rand(0 , 100);
+
+          //Include Function
+?>
